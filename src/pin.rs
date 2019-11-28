77bae0d05143ab9ec84f0634157809c18b47efb3
jgerber <jlgerber@gmail.com>
2019-11-28 14:15:11 -0800

    adds PinBuilder to pin

--- src/pin.rs
+++ src/pin.rs
@@ -6,6 +6,69 @@
  * packybara can not be copied and/or distributed without the express
  * permission of Jonathan Gerber
  *******************************************************/
+pub struct PinBuilder {
+    level: Option<String>,
+    role: Option<String>,
+    platform: Option<String>,
+    site: Option<String>,
+}
+
+impl PinBuilder {
+    fn new() -> PinBuilder {
+        PinBuilder {
+            level: None,
+            role: None,
+            platform: None,
+            site: None,
+        }
+    }
+
+    pub fn level<I: Into<String>>(&mut self, level: I) -> &mut Self {
+        self.level = Some(level.into());
+        self
+    }
+
+    pub fn role<I: Into<String>>(&mut self, role: I) -> &mut Self {
+        self.role = Some(role.into());
+        self
+    }
+
+    pub fn platform<I: Into<String>>(&mut self, platform: I) -> &mut Self {
+        self.platform = Some(platform.into());
+        self
+    }
+
+    pub fn site<I: Into<String>>(&mut self, site: I) -> &mut Self {
+        self.site = Some(site.into());
+        self
+    }
+
+    pub fn build(&mut self) -> Pin {
+        // slight optimization so we dont have to clone all of
+        // those strings. We create a single PinBuilder
+        // and swap references
+        use std::mem;
+        let mut tmp = PinBuilder::new();
+        mem::swap(&mut tmp, self);
+        let PinBuilder {
+            level,
+            role,
+            platform,
+            site,
+        } = tmp;
+        let level = level.unwrap_or("facility".to_string());
+        let role = role.unwrap_or("any".to_string());
+        let platform = platform.unwrap_or("any".to_string());
+        let site = site.unwrap_or("any".to_string());
+        Pin {
+            level,
+            role,
+            platform,
+            site,
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct Pin {
     level: String,
@@ -15,8 +78,23 @@ pub struct Pin {
 }
 
 impl Pin {
+    /// Construct a PinBuilder that you may set properties on
+    /// with individual setters. Once done, you must call `build()`
+    /// to construct the final Pin.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use packybara::Pin;
+    ///
+    /// let pin = Pin::new().level("dev01").role("model").build();
+    /// ```
+    pub fn new() -> PinBuilder {
+        PinBuilder::new()
+    }
+
     /// Construct a new Pin instance
-    pub fn new<I>(level: I, role: I, platform: I, site: I) -> Self
+    pub fn from_parts<I>(level: I, role: I, platform: I, site: I) -> Self
     where
         I: Into<String>,
     {
@@ -55,7 +133,39 @@ mod tests {
 
     #[test]
     fn can_construct_from_strs() {
-        let pin = Pin::new("dev01", "model", "cent7_64", "portland");
+        let pin = Pin::from_parts("dev01", "model", "cent7_64", "portland");
+        assert_eq!(
+            pin,
+            Pin {
+                level: "dev01".to_string(),
+                role: "model".to_string(),
+                platform: "cent7_64".to_string(),
+                site: "portland".to_string(),
+            }
+        );
+    }
+
+    #[test]
+    fn can_construct_from_builder() {
+        let pin = Pin::new().level("dev01").role("model").build();
+        assert_eq!(
+            pin,
+            Pin {
+                level: "dev01".to_string(),
+                role: "model".to_string(),
+                platform: "any".to_string(),
+                site: "any".to_string(),
+            }
+        );
+    }
+    #[test]
+    fn can_construct_all_from_builder() {
+        let pin = Pin::new()
+            .level("dev01")
+            .role("model")
+            .platform("cent7_64")
+            .site("portland")
+            .build();
         assert_eq!(
             pin,
             Pin {
