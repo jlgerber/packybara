4a3a2f39558e84f854332fe61abd02e9608141e4
jgerber <jlgerber@gmail.com>
2019-11-30 00:37:43 -0800

    adds comments to error

--- src/coords_error.rs
+++ src/coords_error.rs
@@ -6,25 +6,31 @@ use strum;
 #[derive(Debug, Snafu)]
 #[snafu(visibility = "pub(crate)")]
 pub enum CoordsError {
+    /// InvalidLevel
     #[snafu(display("Could construct Level from {}", input))]
     InvalidLevel { input: String },
+    /// NewLevelspecError
     #[snafu(display("Error constructing LevelSpec {}: {}", level, source))]
     NewLevelspecError {
         level: String,
         source: Compat<LSpecError>,
     },
+    /// FromStrToSiteError
     #[snafu(display("Error converting string to Site for {}. Error: {}", input, source))]
     FromStrToSiteError {
         input: String,
         source: strum::ParseError,
     },
+    /// FromStrToRoleError
     #[snafu(display("Problem with input string for Role conversion: {}.", input,))]
     FromStrToRoleError { input: String },
+    /// FromStrToPlatformError
     #[snafu(display("Error converting string to Platform for {}. Error: {}", input, source))]
     FromStrToPlatformError {
         input: String,
         source: strum::ParseError,
     },
+    /// DistributionConstructionError
     #[snafu(display("Error distribution. Problem: {}", problem))]
     DistributionConstructionError { problem: &'static str },
 }
