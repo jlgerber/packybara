684e9bbbef86b6ad19c8f85327b9ccec3acf70c5
jgerber <jlgerber@gmail.com>
2019-12-13 07:50:03 -0800

    update register audits with platform table

--- sql/register_audits.sql
+++ sql/register_audits.sql
@@ -3,5 +3,6 @@ SELECT audit.audit_table('package');
 SELECT audit.audit_table('level');
 SELECT audit.audit_table('site');
 SELECT audit.audit_table('role');
+SELECT audit.audit_table('platform');
 SELECT audit.audit_table('distribution'); 
 SELECT audit.audit_table('withpackage');
