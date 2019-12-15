ede78faaead544229e5596c92c334f2657e027c5
jgerber <jlgerber@gmail.com>
2019-12-14 22:06:07 -0800

    updatres PbSet::VersionPins to handle multiple vpins and dists

--- src/bin/cmd/args/set.rs
+++ src/bin/cmd/args/set.rs
@@ -1,19 +1,19 @@
+use packybara::types::IdType;
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug, PartialEq)]
 #[structopt(about = "Set entities in db")]
 pub enum PbSet {
-    /// Set the distribution for a versionpin
+    /// Update one or more existing VersionPins
     #[structopt(display_order = 1)]
     VersionPins {
-        /// provide the versionpin id followed by the distribution id
-        #[structopt(
-            short,
-            long = "dist-id",
-            display_order = 1,
-            min_values = 2,
-            max_values = 2
-        )]
-        dist_ids: Option<Vec<i32>>,
+        /// provide one or more ids of the versionpins(s) we are updating
+        #[structopt(short, long = "vpin-id", display_order = 1)]
+        vpin_ids: Vec<IdType>,
+        /// provide on or more distribution ids to update to. Each id provided
+        /// by the --vpin-id must have a corresponding id set in --dist-id. They
+        /// form pairs.
+        #[structopt(short, long = "dist-id", display_order = 2)]
+        dist_ids: Vec<IdType>,
     },
 }
